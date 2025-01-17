d9895fb791a692b762064c9ee4ceb1368ad686d6
BeliseIrakoze <belisirakoze01@gmail.com>
2025-01-17 14:31:35 +0200

    upadate admin

--- create_admin.py
+++ create_admin.py
@@ -2,25 +2,25 @@
 import sqlite3
 import bcrypt
 from sqlalchemy import select
-from backend.models import User, Base
-from db import get_engine, get_session
+from models import User, Base
+from database import get_engine, get_session
 
 def create_initial_admin():
     session = get_session()
 
     # Check if admin exists
     admin_exists = session.execute(
-        select(User).where(User.username == "admin")
+        select(User).where(User.username == "belise") ### admin user name it can be whatever you want 
     ).scalar_one_or_none()
 
     if admin_exists is None:
         print("Creating admin user...")
-        hashed_password = bcrypt.hashpw("admin".encode(), bcrypt.gensalt())
+        hashed_password = bcrypt.hashpw("123".encode(), bcrypt.gensalt()) ### admin password it can be whatever you want
         admin = User(
-            username="admin",
+            username="Belise", ### change also this 
             hashed_password=hashed_password,
             role="Admin",
-            full_name="System Administrator",
+            full_name="Admin2 Administrator",
             phone_number="0000000",
             age=99
         )
