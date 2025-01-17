0c6354568fc89f6e16ae26fc0813d9dcb9c9834a
BeliseIrakoze <belisirakoze01@gmail.com>
2025-01-17 14:56:54 +0200

    Update app3.py

--- app3.py
+++ app3.py
@@ -218,7 +218,7 @@ def display_image_safely(image_path, use_container_width=True):
         st.info("Image not available")
 
 def home_page():
-    st.title("Welcome to Our Hotel(to be changed) ")
+    st.title("Welcome to Great hotel Kiyovu ")
     st.write("Book your perfect stay with us!")
 
     col1, col2 = st.columns(2)
